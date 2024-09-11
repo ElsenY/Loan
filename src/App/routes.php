22fcbfb9258aaae318bd3fbe7c9c8961cfcd2e98
ElsenY <elsenyacub@yahoo.co.id>
2024-09-12 01:44:08 +0700

    add default route '/'

--- src/App/routes.php
+++ src/App/routes.php
@@ -3,7 +3,14 @@
 use App\Controllers\LoanController;
 use Slim\App;
 use App\Middleware\LoanMiddleware;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
+    $app->get("/", function (Request $request, Response $response): Response { 
+        $response->getBody()->write("hello world");   
+        return $response;
+    });
+
     $app->post('/api/loan',LoanController::class . ':insert')->add(LoanMiddleware::class . ':validateInsert');
 };
